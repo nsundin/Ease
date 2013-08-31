b9bf3315895f2ba79f6c753a9a3aec1c3d598e80
Brandon Olivier <Brandon.Olivier42@gmail.com>
2013-08-30 22:38:19 -0500

    Really deleted files for heroku.

--- backend/models/company_resources/inventory.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-		itemSchema = require('./item').itemSchema;
-
-var inventorySchema = new mongoose.Schema({
-	items: [itemSchema],
-  name: String
-});
-
-mongoose.model('Inventory', inventorySchema);
