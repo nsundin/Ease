d5dd012ecdb11d4153dc0f7af7ec890a82379852
Brandon Olivier <Brandon.Olivier42@gmail.com>
2013-08-31 23:24:09 -0500

    Added more for CORS

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ var allowCrossDomain = function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', config.allowedDomains);
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	if (req.method == 'OPTIONS') {
+		res.send(200)
+	}
 
 	next();
 }
@@ -44,6 +47,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, '/static')));
+app.use(allowCrossDomain);
 
 // Session Cookies
 app.use(express.cookieParser(COOKIE_HASH));
