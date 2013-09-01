e012b15f31fa616cda7f0e0ed0497094e46e59bf
Brandon Olivier <Brandon.Olivier42@gmail.com>
2013-08-31 23:44:07 -0500

    added X-Requested-With to allowed headers for CORS

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose.connect(mongoUri);
 var allowCrossDomain = function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
 	if (req.method == 'OPTIONS') {
 		res.send(200)
 	}
