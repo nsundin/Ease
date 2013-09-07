a08c17af78b67ee0cd111bdeea4bcf25c38fe301
Brandon Olivier <Brandon.Olivier42@gmail.com>
2013-09-06 19:52:11 -0500

    added category and units to item model

--- models/company_resources/item.js
+++ models/company_resources/item.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 var itemSchema = new mongoose.Schema({
 	sku: Number,
 	name: String,
+	units: String,
+	category: String,
 	description: {type: String, default: ''},
 	quantity: {type: Number, default: 0},
 	purchasePrice: Number,
